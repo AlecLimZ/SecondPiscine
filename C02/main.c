190b3345100b30abd9fc2930303f84d3129ac497
Lim Eng chuan <leng-chu@u90z03s08.42kl.edu.my>
2022-09-12 18:08:00 +0800

    look for print_memory

--- C02/main.c
+++ C02/main.c
@@ -6,19 +6,29 @@
 /*   By: leng-chu <-chu@student.42kl.edu.my>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/02 20:25:33 by leng-chu          #+#    #+#             */
-/*   Updated: 2022/08/05 20:23:36 by leng-chu         ###   ########.fr       */
+/*   Updated: 2022/09/12 17:34:19 by leng-chu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "two.h"
 #include <string.h>
 
+int ft_fun(char *s)
+{
+	int	i;
+
+	i = 0;
+	while (*s)
+		i += *s++;
+	return (i);
+}
+
 int	main(int ac, char **av)
 {
-	if (ac == 1)
-		return (0);
-	(void)av;
-	printf("%s\n", "Coucou\ntu vas bien ?");
-	ft_putstr_non_printable("Coucou\ntu vas bien ?");
+	(void) ac;
+	(void) av;
+
+	char *test = "bonjour";
+	ft_print_memory(&test, ft_strlen(test));
 	return (0);
 }
